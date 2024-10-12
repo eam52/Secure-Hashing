7fbe1f33090a31e7a921ebbf742704d10b42b8f8
Ekram <128111692+eam52@users.noreply.github.com>
2024-10-12 13:02:00 -0700

    Update antivirus.py

--- antivirus.py
+++ antivirus.py
@@ -1,4 +1,3 @@
-# importing hashlib for getting sha256() hash function
 import hashlib
 import os
 import magic
@@ -6,29 +5,27 @@ from pyfiglet import Figlet
 import requests
 import sys
 
-# the display_ASCII function will use pyfiglet to create an ASCII
-# Messager for out antivirus script
-
-def ascii_print():
-    fig = Figlet(font= "slant")
-    print(fig.renderText("AntiVirus"))
-
-#The file_hash function will calculate the SHA-256 hash of given file
-# using the hashlib library
 
 ## this method calculates the file hash, and return the calculated hash
 def hashfile(file_path):
+    if len(sys.argv) < 2:
+        print("please provide a file name!")
+        sys.exit(1)
+    file_path = sys.argv[1]
     try:
         with open(file_path, 'rb') as file:
             file_data = file.read()
             hash_code = hashlib.sha256(file_data).hexdigest()
+            return hash_code
     except FileNotFoundError:
         print("This file doesn\'t exist")
-    return hash_code
+        sys.exit(1) # exits if the file is not found
+    except Exception as e:
+        print(f"an error has occured: {e}")
+
 
 ## this method compares the calculated hash with the stored hash to
 ## verfiy integrity and any changes made into the document on file
-
 def compared_hash(file_path, stored_hash):
     current_hash = hashfile(file_path)
     
@@ -37,13 +34,13 @@ def compared_hash(file_path, stored_hash):
     else:
         print(f"Integrity check has failed!!! {current_hash} does not equal to {stored_hash}")
 
-
+        
 def main():
     file = "phishing.docx"
     file_to_hash = hashfile(file)
+    print("the hashed file", file_to_hash)
     print(f"the hashed file: {file_to_hash}")
     compared_hash(file, file_to_hash)
-
 
 if __name__ == "__main__":
     main()
