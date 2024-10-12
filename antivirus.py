daee7a3d634cc4908e74f26e91d3efb91d48885a
eam52 <eam52@uw.edu>
2024-10-12 12:31:59 -0700

    in progress project

--- antivirus.py
+++ antivirus.py
@@ -6,7 +6,6 @@ from pyfiglet import Figlet
 import requests
 import sys
 
-
 # the display_ASCII function will use pyfiglet to create an ASCII
 # Messager for out antivirus script
 
@@ -17,19 +16,34 @@ def ascii_print():
 #The file_hash function will calculate the SHA-256 hash of given file
 # using the hashlib library
 
+## this method calculates the file hash, and return the calculated hash
 def hashfile(file_path):
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
-        hash_code = hashlib.sha256(file_data).hexdigest()
+    try:
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+            hash_code = hashlib.sha256(file_data).hexdigest()
+    except FileNotFoundError:
+        print("This file doesn\'t exist")
     return hash_code
+
+## this method compares the calculated hash with the stored hash to
+## verfiy integrity and any changes made into the document on file
+
+def compared_hash(file_path, stored_hash):
+    current_hash = hashfile(file_path)
+    
+    if current_hash == stored_hash:
+        print("File integrity is successful!" )
+    else:
+        print(f"Integrity check has failed!!! {current_hash} does not equal to {stored_hash}")
 
 
 def main():
     file = "phishing.docx"
     file_to_hash = hashfile(file)
     print(f"the hashed file: {file_to_hash}")
+    compared_hash(file, file_to_hash)
 
-    ascii_print()
 
 if __name__ == "__main__":
     main()
